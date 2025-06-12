2117d39e83177dd2b3898994c2282255347b109e
Lazy-dev-hash <nelzys52@gmail.com>
2025-06-12 17:00:09 +0800

    Update sendMessage.js

--- handles/sendMessage.js
+++ handles/sendMessage.js
@@ -4,43 +4,37 @@ const logger = require('../utils/logger');
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 async function sendApiRequest(requestBody) {
-    try {
-        await axios.post(
-            `https://graph.facebook.com/v15.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
-            requestBody
-        );
-        return true;
-    } catch (error) {
-        logger.error('Failed to send API request:');
-        if (error.response && error.response.data && error.response.data.error) {
-            logger.error(JSON.stringify(error.response.data.error, null, 2));
-        } else {
-            logger.error(error);
-        }
-        return false;
+  try {
+    await axios.post(
+      `https://graph.facebook.com/v16.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+      requestBody
+    );
+  } catch (error) {
+    logger.error('Facebook API Error: Failed to send request.');
+    if (error.response?.data?.error) {
+      logger.error(JSON.stringify(error.response.data.error, null, 2));
     }
+  }
 }
 
-async function sendTypingIndicator(recipientId, isTyping = true) {
-    const requestBody = {
-        recipient: { id: recipientId },
-        sender_action: isTyping ? 'typing_on' : 'typing_off'
-    };
-    await sendApiRequest(requestBody);
+async function sendTypingIndicator(recipientId, action = 'typing_on') {
+  await sendApiRequest({
+    recipient: { id: recipientId },
+    sender_action: action,
+  });
 }
 
-async function sendMessage(recipientId, message) {
-    // Show typing indicator, send message, then turn it off
-    await sendTypingIndicator(recipientId, true);
-    // Add a small delay to make the typing feel more natural
-    await new Promise(resolve => setTimeout(resolve, 500)); 
+async function sendMessage(recipientId, messagePayload) {
+  await sendTypingIndicator(recipientId, 'typing_on');
+  await new Promise(resolve => setTimeout(resolve, 500)); // Natural delay
 
-    const requestBody = {
-        recipient: { id: recipientId },
-        message: message
-    };
-    await sendApiRequest(requestBody);
-    await sendTypingIndicator(recipientId, false);
+  await sendApiRequest({
+    recipient: { id: recipientId },
+    message: messagePayload,
+    messaging_type: 'RESPONSE',
+  });
+
+  await sendTypingIndicator(recipientId, 'typing_off');
 }
 
 module.exports = { sendMessage };
